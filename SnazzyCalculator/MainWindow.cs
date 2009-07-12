a1ef14d12431f8f55e02d8a615cefdd76857942b
Sarah Vessels <cheshire137@gmail.com>
2009-07-12 09:42:45 -0400

    Wrapping MainWindow's in SnazzyCalculator namespace.  Adding necessary reference to Gui.csproj.

--- SnazzyCalculator/MainWindow.cs
+++ SnazzyCalculator/MainWindow.cs
@@ -1,29 +1,32 @@
 using Gtk;
 using Gui;
 
-public partial class MainWindow: Window
+namespace SnazzyCalculator
 {
-    private static IGui _gui;
-
-    public static void Main(string[] args)
-    {
-#if OSX
-        _gui = new CocoaSharp();
-#elif LINUX
-        _gui = new Gui.GtkSharp();
-#endif
-        _gui.PopulateWindow();
-        _gui.ShowWindow();
-    }
-
-    public MainWindow(): base (WindowType.Toplevel)
-    {
-        Build();
-    }
-    
-    protected void onDeleteEvent(object sender, DeleteEventArgs a)
-    {
-        Application.Quit();
-        a.RetVal = true;
-    }
+	public partial class MainWindow: Window
+	{
+	    private static IGui _gui;
+	
+	    public static void Main(string[] args)
+	    {
+			#if OSX
+	        _gui = new CocoaSharp();
+			#elif LINUX
+	        _gui = new Gui.GtkSharp();
+			#endif
+	        _gui.PopulateWindow();
+	        _gui.ShowWindow();
+	    }
+	
+	    public MainWindow(): base (WindowType.Toplevel)
+	    {
+	        Build();
+	    }
+	    
+	    protected void onDeleteEvent(object sender, DeleteEventArgs a)
+	    {
+	        Application.Quit();
+	        a.RetVal = true;
+	    }
+	}
 }
